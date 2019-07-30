b0d5f15118a78b408d29f659f2e8eeeec2181c8d
fxola <afolabiabass62@gmail.com>
2019-07-30 12:39:22 +0100

    chore(redux): Setup Redux - install necessary dependencies - setup root reducer file - setup store configuration - add sample action type template - connect app to redux - finishes [#166816053]

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { hot } from 'react-hot-loader';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import LandingPage from './views/LandingPage/LandingPage';
 import LoginPage from './views/LoginPage/LoginPage';
+import { Provider } from 'react-redux';
+import setupStore from '../src/store';
 
+const store = setupStore();
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <Router>
         <ul>
           <li>
@@ -21,6 +25,7 @@ class App extends Component {
           <Route path="/login" component={LoginPage} />
         </switch>
       </Router>
+      </Provider>
     );
   }
 }
