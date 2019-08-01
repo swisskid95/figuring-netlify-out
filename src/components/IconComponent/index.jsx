3fce104e772ccc1859abeee7738480e579b943f7
Sanusi Babatunde <swisskid95@gmail.com>
2019-08-01 15:33:20 +0100

    chore(reusable-component): add button component
    add style for normal button
    add style for inverse button type
    
    [Starts #167621041]'

--- src/components/IconComponent/index.jsx
+++ src/components/IconComponent/index.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import PropTypes from 'proptypes';
+import PropTypes from 'prop-types';
 import './index.scss';
 
-
-/** 
+/**
  *  Sample Usage
  *  <IconComponent
  *      src={'./../src/assets/images/twitter-dark.svg'}
  *      alt={'image asset'}
  *      className={'icon-large'}
  *  />
-*/
+ */
 const IconComponent = ({ className, src, alt }) => {
   return <img className={className} src={src} alt={alt} />;
 };
-
-
 
 IconComponent.propTypes = {
   className: PropTypes.string.isRequired,
