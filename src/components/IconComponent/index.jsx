19da85d879bd58b7102854af802daf66e8cb58ce
devPinheiro <pinheirolaoluwa@gmail.com>
2019-08-01 15:59:19 +0100

    chore(footer): create footer component add footer styles add footer component write tests [#167619967]

--- src/components/IconComponent/index.jsx
+++ src/components/IconComponent/index.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import PropTypes from 'proptypes';
 import './index.scss';
 
-
-/** 
+/**
  *  Sample Usage
  *  <IconComponent
  *      src={'./../src/assets/images/twitter-dark.svg'}
  *      alt={'image asset'}
  *      className={'icon-large'}
  *  />
-*/
+ */
 const IconComponent = ({ className, src, alt }) => {
   return <img className={className} src={src} alt={alt} />;
 };
-
-
 
 IconComponent.propTypes = {
   className: PropTypes.string.isRequired,
